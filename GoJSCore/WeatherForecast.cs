4092ad5a943b1b84c5da77a6877bcc5ef56b9efb
promlert pearrukkrai <promlertp@gmail.com>
2021-09-12 18:04:47 +0700

    adsd

--- GoJSCore/WeatherForecast.cs
+++ GoJSCore/WeatherForecast.cs
@@ -12,7 +12,6 @@ namespace GoJSCore
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string Summary { get; set; }
     }
 }
